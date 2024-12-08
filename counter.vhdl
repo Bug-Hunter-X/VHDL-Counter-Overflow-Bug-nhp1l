cecf4b906e4c19b528b94d4bc77a52b5054e9a6d
BugHunter-X <askzenai@gmail.com>
2024-12-08 11:48:06 +0100

    Add counter.vhdl file

---
+++ counter.vhdl
@@ -0,0 +1,23 @@
+```vhdl
+ENTITY counter IS
+  PORT (
+    clk : IN  STD_LOGIC;
+    rst : IN  STD_LOGIC;
+    count : OUT INTEGER RANGE 0 TO 15
+  );
+END ENTITY;
+
+ARCHITECTURE behavioral OF counter IS
+  SIGNAL temp_count : INTEGER RANGE 0 TO 15 := 0;
+BEGIN
+  PROCESS (clk, rst)
+  BEGIN
+    IF rst = '1' THEN
+      temp_count <= 0;
+    ELSIF rising_edge(clk) THEN
+      temp_count <= temp_count + 1;
+    END IF;
+  END PROCESS;
+  count <= temp_count;
+END ARCHITECTURE;
+```
